491cc5874f9302e646290378eb59fff070fb9494
ACC1100 <aaron.chao2000@gmail.com>
2021-03-14 18:01:17 +1100

    fc

--- 24solutions/src/index.js
+++ 24solutions/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
+    <ColorModeScript />
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
